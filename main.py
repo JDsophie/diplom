02dce3bd0ba2a71697559659310ca5baca0f9e82
fridman_ms <marina.s.fridman@gmail.com>
2024-11-17 18:50:49 +0300

    * рефакторинг

--- main.py
+++ main.py
@@ -1,22 +1,26 @@
-from gale_shapley import generate_data, gale_shapley
+from gale_shapley import gale_shapley, generate_data
 
 num_students = 10
 num_universities = 5
 k = 2
-   
-students, universities, quotas, preferences_s, preferences_u = generate_data(num_students, num_universities, k)
-   
+
+students, universities, quotas, preferences_s, preferences_u = generate_data(
+    num_students, num_universities, k
+)
+
 print("Студенты:", students)
 print("Университеты:", universities)
 print("Квоты:", quotas)
 print("Предпочтения студентов:", preferences_s)
 print("Предпочтения университетов:", preferences_u)
 
-matches, student_matches, unmatchedstudents = gale_shapley(num_students, num_universities, quotas, preferences_s, preferences_u)
-   
+university_matches, student_matches, unmatched_students = gale_shapley(
+    num_students, num_universities, quotas, preferences_s, preferences_u
+)
+
 print("Сопоставление университетов и студентов:")
-print(matches)
+print(university_matches)
 print("Сопоставление студентов и университетов:")
 print(student_matches)
 print("Незачисленные студенты:")
-print(unmatchedstudents)
+print(unmatched_students)
